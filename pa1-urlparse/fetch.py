c0a4ac5a3b7dac1c66247fe38e505549a30f7d2f
Gregory Gundersen <ggundersen@gmail.com>
2014-07-26 13:59:48 -0400

    Committing solution for urlparse()

--- pa1-urlparse/fetch.py
+++ pa1-urlparse/fetch.py
@@ -28,7 +28,7 @@ def urlparse(url):
         |           |            |            |        |
        scheme     authority     path        query   fragment
 
-    For details, see: http://tools.ietf.org/html/rfc3986#section-3.1
+    For details, see: http://tools.ietf.org/html/rfc3986
 
     For example,
 
@@ -38,4 +38,109 @@ def urlparse(url):
     '''
     scheme = netloc = path = params = query = fragment = ''
 
+    # Handle scheme
+    i = url.find(':')
+    if i > 0:
+        url, scheme = _parse_scheme(url, i)
+
+    # Handle authority / netloc / domain
+    if url[:2] == '//':
+        url, netloc = _parse_netloc(url)
+
+    # Handle port
+    i = url.find(':')
+    if i > 0:
+        url = _parse_port(url, i)
+
+    # Handle path
+    url, path = _parse_path(url)
+  
+    # Handle query string and fragment
+    if len(url) > 0 and url[0] == '?':
+        query, fragment = _parse_query(url)
+
     return ParseResult(scheme, netloc, path, params, query, fragment)
+
+
+def _parse_scheme(url, i):
+    '''
+    "Scheme names consist of a sequence of characters beginning with a letter
+    and followed by any combination of letters, digits, plus ("+"), period
+    ("."), or hyphen ("-")."
+    Check for port
+    '''
+    if url[i+1].isdigit():
+        scheme = ''
+    else:
+        scheme = url[:i].lower()
+        url = url[i+1:]
+    return url, scheme
+
+
+def _parse_netloc(url):
+    '''
+    "The authority component is preceded by a double slash ("//") and is
+    terminated by the next slash ("/"), question mark ("?"), or number sign
+    ("#") character, or by the end of the URI.
+    '''
+    url = url[2:]
+    minI = None
+    for c in '/?#':
+        i = url.find(c)
+        if i > 0 and (minI == None or i < minI):
+            minI = i
+    if minI != None:
+        netloc = url[:minI]
+        url = url[minI:]
+    else:
+        netloc = url
+        url = ''
+    return url, netloc
+
+
+def _parse_path(url):
+    '''
+    "The path is terminated by the first question mark ("?") or number sign
+    ("#") character, or by the end of the URI."
+    '''
+    minI = None
+    for c in '?#':
+        i = url.find(c, 1)
+        if i > 0 and (minI == None or i < minI):
+            minI = i
+    if minI != None:
+        path = url[:minI]
+        url = url[minI:]
+    else:
+        path = url
+        url = ''
+    return url, path
+
+
+def _parse_port(url, i):
+    while url[i+1].isdigit():
+        i = i+1
+    return url[:i]
+
+
+def _parse_query(url):
+    '''
+    "The query component is indicated by the first question mark ("?")
+    character and terminated by a number sign ("#") character or by the end
+    of the URI."
+
+    "A fragment identifier component is indicated by the presence of a number
+    sign ("#") character and terminated by the end of the URI."
+    '''
+    # +1 to remove "?"
+    query = url[1:]
+    j = query.find('#', 1)
+    if j > 1:
+        # +1 to remove "#"
+        fragment = query[j+1:]
+        query = query[:j]
+    else:
+        fragment = ''
+    return query, fragment
+
+
